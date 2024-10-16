4da07c9e285d21434b12154837c820c2d0d74963
Kauan Rodrigues Lima <162514268+Kauanrodrigues01@users.noreply.github.com>
2024-10-15 22:15:15 -0300

    update

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("products", views.api_products)
+    path('products', views.api_products),
+    path('products/<str:pk>', views.api_product_detail)
 ]
--- api/views.py
+++ api/views.py
@@ -1,15 +1,23 @@
-import django.db.models.signals
 from django.shortcuts import render
-import rest_framework
 from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
 from .serializers import ProductSerializer
 from storeapp.models import Product
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.exceptions import NotFound
 
 @api_view()
 def api_products(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view()
+def api_product_detail(request, pk):
+    try:
+        product = Product.objects.get(id=pk)
+    except Product.DoesNotExist:
+        raise NotFound({'detail': 'product not found'})
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
