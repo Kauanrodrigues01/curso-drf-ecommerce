223464aa2191b194b5e0fb16cd749ef24fbd3af5
Kauan Rodrigues Lima <162514268+Kauanrodrigues01@users.noreply.github.com>
2024-10-19 20:48:31 -0300

    test: adicionar verificação de erro ao criar usuário com e-mail inválido

--- api/tests/tests_users/test_user_views.py
+++ api/tests/tests_users/test_user_views.py
@@ -5,6 +5,7 @@ from api.tests.tests_users.base.test_base_user_views import TestBaseUserViews
 from django.urls import reverse
 
 class TestUserViews(TestBaseUserViews):
+    # TEST CREATE
     def test_create_user(self):
         response = self.client.post(self.url_register_user, self.data_user)
         self.assertTrue(User.objects.filter(email=self.data_user['email']))
@@ -15,12 +16,19 @@ class TestUserViews(TestBaseUserViews):
         self.assertTrue(User.objects.filter(email=self.data_user['email']))
         self.assertTrue(Customer.objects.filter(email=self.data_user['email']))
         self.assertEqual(response.status_code, 201)
-        
+    
+    # TEST EMAIL FIELD
     def test_if_an_error_appears_when_trying_to_create_the_user_without_the_email(self):
         del self.data_user['email']
         response = self.client.post(self.url_register_user, self.data_user)
         self.assertEqual(response.status_code, 400)
         
+    def test_if_any_error_appears_when_trying_to_create_the_user_with_an_invalid_email(self):
+        self.data_user['email'] = 'InvalidEmail@.com'
+        response = self.client.post(self.url_register_user, self.data_user)
+        self.assertEqual(response.status_code, 400)
+    
+    # TEST FIRST NAME FIELD   
     def test_if_an_error_appears_when_trying_to_create_the_user_without_the_first_name(self):
         del self.data_user['first_name']
         response = self.client.post(self.url_register_user, self.data_user)
@@ -35,7 +43,8 @@ class TestUserViews(TestBaseUserViews):
         self.data_user['first_name'] = 'a' * 101
         response = self.client.post(self.url_register_user, self.data_user)
         self.assertEqual(response.status_code, 400)
-        
+    
+    # TEST LAST NAME FIELD 
     def test_if_an_error_appears_when_trying_to_create_the_user_without_the_last_name(self):
         del self.data_user['last_name']
         response = self.client.post(self.url_register_user, self.data_user)
@@ -50,7 +59,8 @@ class TestUserViews(TestBaseUserViews):
         self.data_user['last_name'] = 'a' * 101
         response = self.client.post(self.url_register_user, self.data_user)
         self.assertEqual(response.status_code, 400)
-        
+    
+    # TEST PASSWORD AND PASSWORD_CONFIRMATION FIELDS
     def test_if_an_error_appears_when_trying_to_create_the_user_without_the_password(self):
         del self.data_user['password']
         response = self.client.post(self.url_register_user, self.data_user)
